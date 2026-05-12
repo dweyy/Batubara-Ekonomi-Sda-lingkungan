df239b938f70b3776b70e728c67cd32fa423564c
dweyy <dwiranisalfatihah06@gmail.com>
2026-05-12 23:03:19 +0700

    Tambah sidebar info anggota dan bimbingan
    Menambahkan sub-judul "Ekonomi SDA & Lingkungan" di sidebar
    Menampilkan daftar anggota tim dan pembimbing

--- app.py
+++ app.py
@@ -66,7 +66,16 @@ def load_data():
     df["MC_per_ton"]  = df["mc"]  / (df["Q"] * 1e6)
     return df 
 df = load_data()
-
+# ─────────────────────────────
+# SIDEBAR – INFORMASI TIM
+# ─────────────────────────────
+st.sidebar.markdown("### 🌿 Ekonomi SDA & Lingkungan")
+st.sidebar.markdown("**Nama Anggota:**")
+st.sidebar.markdown("- Dwirani Salfatihah")
+st.sidebar.markdown("- Nazwa Aprilia Putri")
+st.sidebar.markdown("- Annisa Tri Lestari")
+st.sidebar.markdown("**Bimbingan:** Yuhka Sundaya")
+st.sidebar.markdown("---")  # garis pemisah
 # ─────────────────────────────────────────────
 # SIDEBAR – PARAMETER SIMULASI
 # ─────────────────────────────────────────────
